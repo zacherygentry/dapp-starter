302cccc6c4fc9cdaa5b342edf87ff9d9320ab9b6
Zachery Gentry <zacherygentry@gmail.com>
2021-08-15 23:50:46 -0500

    restructure

--- truffle-config.js
+++ truffle-config.js
@@ -6,8 +6,8 @@ module.exports = {
       network_id: '*', // Match any network id
     },
   },
-  contracts_directory: './ethereum/contracts/',
-  contracts_build_directory: './ethereum/abis/',
+  contracts_directory: './contracts/',
+  contracts_build_directory: './abis/',
   compilers: {
     solc: {
       optimizer: {
